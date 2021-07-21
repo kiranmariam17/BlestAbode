a5873ddc74af83f44c4995761f6e948923f7bc7b
Kiran Mariam George <47290785+kiranmariam17@users.noreply.github.com>
2021-07-21 23:29:21 +0530

    lheader component upload

---
+++ Frontend/src/app/lheader/lheader.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LheaderComponent } from './lheader.component';
+
+describe('LheaderComponent', () => {
+  let component: LheaderComponent;
+  let fixture: ComponentFixture<LheaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LheaderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LheaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
